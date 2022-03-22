a86030871861e57ce553c2fe0d1bb8c02a6e34c4
louisa <mensahlouuisa39@gmail.com>
2022-03-22 14:44:52 +0000

    react-component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
 import writers from './writers';
+import { ProfileCard } from './ProfileCard';
+
 function App() {
   return (
     <div>
       <h1>write profiles</h1>
       <div className="container">
         {writers.map(writer => (
-
-        <div className="card">
-          <img src={`/images/${writer.avatar}.png`}
-            width="300px"
-            height="300px"
-            alt=""
-          />
-          <div className='textGroup'>
-          <h3>{writer.name}</h3>
-          <p>{writer.email}</p>
-          <p>{writer.phone}</p>
-
+          <ProfileCard writer={writer}/>
+          ))}
           </div>
-        </div>
+          </div>
+/
+        {/* //     width="300px"
+        //     height="300px"
+        //     alt=""
+        //   />
+        //   <div className='textGroup'>
+        //   <h3>{writer.name}</h3>
+        //   <p>{writer.email}</p>
+        //   <p>{writer.phone}</p>
+
+        //   </div>
+        // </div>
      ))}
       </div>
-    </div>
+    </div> */}
 
   );
 }
