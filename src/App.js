7b8cfe58b685414509417c99805d701d0d20c2ff
louisa <mensahlouuisa39@gmail.com>
2022-03-24 13:53:49 +0000

    React components 4

--- src/App.js
+++ src/App.js
@@ -1,32 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 // import writers from './writers';
-import { ProfileCard } from './ProfileCard';
+import ProfileCard  from './ProfileCard';
 import { useEffect, useState } from 'react';
 
 
 function App() {
-  const[writers, setWriters] = useState([]);
+  const[data, setData] = useState({
+    writers: [],
+    loading: false,
+  });
 
-  useEffect(() => {
+const handleClick = () => {
+  setData(prevData => ({
+    ...prevData,
+    loading: true,
+  }));
+}
+  setTimeout(() => {
     const getwriters = async () => {
       const response = await fetch("/writers.json");
       const data= await response.json();
       console.log (data)
-      setWriters(data)
+      setData({
+        writers: data,
+        loading: false,
+      });
       
     };
     getwriters();
-   }, []);
+   }, 2000);
+
+   if(data.loading) {
+     return(
+       <div>
+         <h1>Writer Profiles</h1>
+         <div className='continer'>
+           <div className='Card action'>
+             <p className='infoText'>loading... </p>
+
+           </div>
+         </div>
+       </div>
+     );
+  }     
   
   return (
     <div>
       <h1>write profiles</h1>
 
       <div className="container">
-        {writers.map((writer) => (
-          <ProfileCard writer={writer}/>
-          ))}
+        {data.writers.length=== 0 ? (
+      <div className='card action'>
+        <p className='infoText'> oops... no writer profile found</p>
+        <button className='actionBtn' onClick={handleClick}>
+          Get writers
+          </button>
+          </div>
+        ) : (
+          data.writers.map((writer) => (
+          <ProfileCard key={writer.id} writer={writer}/>
+          ))
+        )}
       </div>
     </div>
 
