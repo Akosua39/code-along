43a48f459176a36284e7be3b76b9aecf11cd59a9
louisa <mensahlouuisa39@gmail.com>
2022-04-04 14:26:55 +0000

    react component 8

---
+++ src/pages/Blog.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+   <section>
+       <h1> Welcome to the Blog Page</h1>
+   <p>
+       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, nobis facere quas laborum et vitae natus sunt aspernatur nesciunt doloribus, temporibus dolor eius cum iure, laboriosam obcaecati aliquid quae ipsum!
+       Soluta cum, porro praesentium ex harum non dolorum dignissimos doloribus provident amet unde est dicta perspiciatis eligendi, voluptatum odit aspernatur mollitia animi consequuntur exercitationem commodi delectus, minima necessitatibus! Labore, temporibus.
+       Adipisci enim distinctio, delectus consequatur placeat itaque cumque, nesciunt necessitatibus temporibus harum veniam deserunt. Officia dicta dolorem ducimus, harum vel delectus, blanditiis commodi assumenda modi nesciunt aspernatur. Suscipit, fugiat libero!
+   </p>
+   <p>
+       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, nobis facere quas laborum et vitae natus sunt aspernatur nesciunt doloribus, temporibus dolor eius cum iure, laboriosam obcaecati aliquid quae ipsum!
+       Soluta cum, porro praesentium ex harum non dolorum dignissimos doloribus provident amet unde est dicta perspiciatis eligendi, voluptatum odit aspernatur mollitia animi consequuntur exercitationem commodi delectus, minima necessitatibus! Labore, temporibus.
+       Adipisci enim distinctio, delectus consequatur placeat itaque cumque, nesciunt necessitatibus temporibus harum veniam deserunt. Officia dicta dolorem ducimus, harum vel delectus, blanditiis commodi assumenda modi nesciunt aspernatur. Suscipit, fugiat libero!
+   </p>
+   <p>
+       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, nobis facere quas laborum et vitae natus sunt aspernatur nesciunt doloribus, temporibus dolor eius cum iure, laboriosam obcaecati aliquid quae ipsum!
+       Soluta cum, porro praesentium ex harum non dolorum dignissimos doloribus provident amet unde est dicta perspiciatis eligendi, voluptatum odit aspernatur mollitia animi consequuntur exercitationem commodi delectus, minima necessitatibus! Labore, temporibus.
+       Adipisci enim distinctio, delectus consequatur placeat itaque cumque, nesciunt necessitatibus temporibus harum veniam deserunt. Officia dicta dolorem ducimus, harum vel delectus, blanditiis commodi assumenda modi nesciunt aspernatur. Suscipit, fugiat libero!
+   </p>
+
+
+   </section>
+  )
+}
+
+export default Blog
